32d659d4c01567f91995028dc27c52e32ada88e0
Pablo Nuñez <pabloing1993@gmail.com>
2024-06-03 16:17:45 -0300

    Creating tables.sql

---
+++ database_tables.sql
@@ -0,0 +1,10 @@
+create table pabloing1993_coderhouse.bitcoin_candles(
+	open_time DATETIME,
+	open_price FLOAT,
+	high_price FLOAT,
+	low_price FLOAT,
+	close_price FLOAT,
+	volume FLOAT,
+	close_time DATETIME,
+	trades INT
+);
